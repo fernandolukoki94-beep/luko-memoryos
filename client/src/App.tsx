b856db92bb935c14700217073dbc76bac564e4a0
fernandolukoki94-beep <2.77207774e+08+fernandolukoki94-beep@users.noreply.github.com>
2026-08-15 15:20:26 +0000

    revert: keep REX exclusively in rex-os-observability

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,13 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import GamePage from "@/pages/GamePage";
-import { Route, Router as WouterRouter, Switch } from "wouter";
+import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import { MemoriesProvider } from "./contexts/MemoriesContext";
 import Home from "./pages/Home";
 import LandingPage from "./pages/LandingPage";
-import RexLanding from "./pages/RexLanding";
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
 import ProfileTimeline from "./pages/ProfileTimeline";
@@ -20,13 +19,9 @@ import RexOperations from "./pages/RexOperations";
 
 
 function Router() {
-  const basePath = import.meta.env.BASE_URL.replace(/\/$/, "");
-
   return (
-    <WouterRouter base={basePath}>
-      <Switch>
-      <Route path={"/"} component={RexLanding} />
-      <Route path={"/memoryos"} component={LandingPage} />
+    <Switch>
+      <Route path={"/"} component={LandingPage} />
       <Route path={"/home"} component={Home} />
       <Route path={"/game"} component={GamePage} />
       <Route path={"/auth"} component={Auth} />
@@ -38,9 +33,8 @@ function Router() {
       <Route path={"/rex"} component={RexOperations} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
-        <Route component={NotFound} />
-      </Switch>
-    </WouterRouter>
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
