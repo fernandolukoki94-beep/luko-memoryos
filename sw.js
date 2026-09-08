2449430bb0ad59b32c7d1923fbd5d5c0ccf60821
fernandolukoki94-beep <2.77207774e+08+fernandolukoki94-beep@users.noreply.github.com>
2026-09-08 14:52:49 +0000

    feat: make cyberhub interactive and installable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cyberhub-v2";
+const CACHE = "cyberhub-v1";
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", (event) => {
